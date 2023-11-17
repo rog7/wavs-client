8481f75effd728c519b25a8077a85a89878846bd
rogersimon <roger@circleinapp.com>
2023-11-17 09:24:57 -0500

    server urls

--- utils/globalVars.ts
+++ utils/globalVars.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "development"
-    ? "https://vault-server-jxzx.onrender.com"
-    : "http://54.235.12.97:9000";
+    ? "https://ecc5-67-216-154-211.ngrok-free.app"
+    : "https://vault-server-jxzx.onrender.com:10000";
 
 export const STRIPE_PAYMENT_LINK =
   process.env.NODE_ENV === "development"
