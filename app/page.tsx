12ff9e9e893c5cf9cbe41a773146bebc6cd1cd22
rogersimon <roger@circleinapp.com>
2023-11-17 13:12:48 -0500

    changed url

--- app/page.tsx
+++ app/page.tsx
@@ -246,7 +246,7 @@ export default function Home() {
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
-                    className="rounded-md bg-[#0070F3] px-3.5 py-2.5 text-md font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                    className="cursor-pointer rounded-md bg-[#0070F3] px-3.5 py-2.5 text-md font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                     onClick={() => {
                       router.push("/sign-up");
                     }}
