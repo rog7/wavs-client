be0aff04023f4f8397db50887a1c5282b3bfb7c2
rogersimon <roger@circleinapp.com>
2023-11-18 08:56:47 -0500

    updated prod api url

--- utils/globalVars.ts
+++ utils/globalVars.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "https://ecc5-67-216-154-211.ngrok-free.app"
-    : "http://api.usewavs.com:9000"
+    : "https://wavs.onrender.com"
 
 export const STRIPE_PAYMENT_LINK =
   process.env.NODE_ENV === "development"
